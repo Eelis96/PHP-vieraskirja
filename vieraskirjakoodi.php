996992ea01249fb113ae0120e8423adc8401efeb
Eelis96 <57720310+Eelis96@users.noreply.github.com>
2020-08-31 11:27:57 +0300

    Delete vieraskirjakoodi.php

--- vieraskirjakoodi.php
+++
@@ -1,9 +0,0 @@
-<?php
-if(isset($_POST['poista'])){
-    $tyhja = "";
-    file_put_contents("vieraat.txt", $tyhja);
-    header('location:hallintapaneeli.php?success');
-}else{
-    header('location:hallintapaneeli.php');
-}
-?>
